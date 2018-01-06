d4a0637369c118181506805bbbb54aa6e90edcf3
Abhinav Verma <abhicsmnnit@gmail.com>
2018-01-06 18:06:00 +0530

    Added POST /todos and saved the todo to MongoDB

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const {User} = require('./models/User');
 
 const app = express();
 
+// For parsing json coming in our requests
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        console.log('Saved!');
+        res.send(doc);
+    }, (error) => {
+        console.log('Unable to save!');
+        res.status(400).send(error);
+    });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
