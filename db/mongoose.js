1a076a865c6f61987293a99bfbf7cf466613f8ef
Abhinav Verma <abhicsmnnit@gmail.com>
2018-01-07 01:23:24 +0530

    Deploying at Heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 // Tell Mongoose to use the JS Promises (not any other 3rd-Party Promise Library like BlueBird)
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
