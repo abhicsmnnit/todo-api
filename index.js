21167591678322cc71f4cf80bac03f243b02540c
Abhinav Verma <abhicsmnnit@gmail.com>
2018-01-07 22:47:53 +0530

    Added PATCH /todos/:id route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const mongoose = require('./db/mongoose');
 const {Todo} = require('./models/Todo');
@@ -59,6 +60,46 @@ app.delete('/todos/:id', (req, res) => {
         res.send({ todo });
     }).catch((err) => res.status(400).send());
 });
+
+app.patch('/todos/:id', (req, res) => {
+    const updatedTodoId = req.params.id;
+    if (!ObjectID.isValid(updatedTodoId)) {
+        return res.status(404).send();
+    }
+
+    const body = _.pick(req.body, ['text', 'completed']);
+    const errors = validate(body);
+    if (_.size(errors) > 0) {
+        return res.status(400).send({ errors });
+    }
+
+    if (body.completed === false) {
+        body.completedAt = null;
+    } else if (body.completed === true) {
+        body.completedAt = Math.floor(Date.now() / 1000);
+    }
+
+    Todo.findByIdAndUpdate(updatedTodoId, { $set: body }, { new: true })
+        .then((todo) => {
+            if (!todo) {
+                res.status(404).send();
+            }
+            res.send({ todo });
+        }).catch(err => res.status(400).send());
+});
+
+const validate = (body) => {
+    const errors = {};
+    if (_.has(body, 'text') && !isValidText(body.text)) {
+        _.set(errors, 'text', 'must be a non-empty string');
+    }
+    if (_.has(body, 'completed') && !_.isBoolean(body.completed)) {
+        _.set(errors, 'completed', 'must be a boolean');
+    }
+    return errors;
+};
+
+const isValidText = (text) => _.isString(text) && text.trim().length > 0;
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
